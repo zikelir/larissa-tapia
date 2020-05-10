7af057a2715a0fb622ca37a272aba6f73c8498ed
zikelir <pvselano@gmail.com>
2020-05-10 16:52:24 -0300

    fixed sizing for test.

--- src/components/homepage/testimonials/TestimonialElement.vue
+++ src/components/homepage/testimonials/TestimonialElement.vue
@@ -6,8 +6,8 @@
         <div>{{testimonial.age}}</div>
         <div>{{testimonial.job}}</div>
       </div>
-      <div class="testmonial-text">{{testimonial.testimonial}}</div>
-      <div class="read-more-testmonial">Ler mais +</div>
+      <p class="testmonial-text">"{{testimonial.testimonial}}"</p>
+      <!-- <div class="read-more-testmonial">Ler mais +</div> -->
     </div>
 </template>
 
@@ -37,7 +37,61 @@ export default {
        flex-direction: column;
        margin-right: 1em;
        border-radius: 4px;
-       background-color: #FFF5F7;;
+       background-color: #FFF5F7;
+       margin-left: 1em;
+     }
+
+     .testmonial-photo {
+       background-repeat: no-repeat;
+       background-size: cover;
+       background-position: center;
+       min-width: 100%;
+       height: 240px;
+       border-radius: 4px 4px 0 0;
+     }
+
+    .testmonial-personal-info {
+      border-top: 8px solid #FFCCD4;
+      background-color: #FFEDF0;
+      color: #670C1B;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 16px;
+    }
+
+    .testmonial-name {
+      font-weight: 500;
+    }
+
+    .testmonial-text {
+      display: flex;
+      padding: 0em 1em 0 1em;
+      white-space: pre-line;
+      line-height: 1.4;
+    }
+
+    .read-more-testmonial {
+      font-weight: bold;
+      text-align: center;
+      color: #670C1B;
+      margin: 1em 0 1.5em 0;
+      font-size: 12px;
+    }
+}
+
+/* tablet */
+@media (min-width: 768px) {
+    #testimonial-card {
+       width: 320px;
+       box-sizing: border-box;
+       display: flex;
+       flex-direction: column;
+       margin-right: 1em;
+       border-radius: 4px;
+       background-color: #FFF5F7;
+       margin-left: 1em;
      }
 
      .testmonial-photo {
@@ -67,9 +121,8 @@ export default {
     .testmonial-text {
       display: flex;
       padding: 1em;
-      overflow-wrap: break-word;
-      word-break: break-all;
       white-space: pre-line;
+      line-height: 1.4;
     }
 
     .read-more-testmonial {
@@ -81,14 +134,57 @@ export default {
     }
 }
 
-/* tablet */
-@media (min-width: 768px) {
-    
-}
-
 /* web */
 @media (min-width: 1440px) {
-   
+   #testimonial-card {
+       width: 320px;
+       box-sizing: border-box;
+       display: flex;
+       flex-direction: column;
+       margin-right: 1em;
+       border-radius: 4px;
+       background-color: #FFF5F7;
+       margin: 1em;
+     }
+
+     .testmonial-photo {
+       background-repeat: no-repeat;
+       background-size: cover;
+       background-position: center;
+       min-width: 100%;
+       height: 240px;
+       border-radius: 4px 4px 0 0;
+     }
+
+    .testmonial-personal-info {
+      border-top: 8px solid #FFCCD4;
+      background-color: #FFEDF0;
+      color: #670C1B;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 16px;
+    }
+
+    .testmonial-name {
+      font-weight: 500;
+    }
+
+    .testmonial-text {
+      display: flex;
+      padding: 1em;
+      white-space: pre-line;
+      line-height: 1.4;
+    }
+
+    .read-more-testmonial {
+      font-weight: bold;
+      text-align: center;
+      color: #670C1B;
+      margin: 1em 0 1.5em 0;
+      font-size: 12px;
+    }
 }
 
 
