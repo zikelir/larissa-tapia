16b10bb0f34ea7c36628327bba8582cf683677a4
zikelir <pvselano@gmail.com>
2020-05-23 21:29:41 -0300

    modal for treatments improvement

--- src/components/homepage/treatments/Treatments.vue
+++ src/components/homepage/treatments/Treatments.vue
@@ -7,40 +7,70 @@
                   <div class="treatment-element" id="estetica-dental" v-on:click="show('estetica')"><div class="treatment-title">Estética Dental</div></div>
                   <div class="treatment-element" id="dor-orofacial" v-on:click="show('dtm')"><div class="treatment-title">DTM e Dores Orofaciais</div></div>
                   <div class="treatment-element" id="invisalign" v-on:click="show('invisalign')"><div class="treatment-title">Aparelho Invisível</div></div>
-                  <div class="treatment-element" id="implantes" v-on:click="show('implantes')"><div class="treatment-title">Implantes</div></div>
+                  <div class="treatment-element" id="implantes" v-on:click="show('implante')"><div class="treatment-title">Implantes</div></div>
                   <div class="treatment-element" id="laserterapia" v-on:click="show('laserterapia')"><div class="treatment-title">Laserterapia</div></div>
                   <!-- <div class="treatment-element" id="clareamento"><div class="treatment-title">Clareamento</div></div> -->
                </div>
 
-               <modal name="harmonizacao" class="treatment-modal" :adaptive="true" width="100%" height="100%">
-                     <h1>{{treatments.harmonizacao.title}}</h1>
-                     <div>{{treatments.harmonizacao.text}}</div>
-                    <button  v-on:click="hide('harmonizacao')">close</button>
+               <modal name="harmonizacao" :adaptive="true" width="100%" height="100%">
+                    <div class="treatment-modal">
+                         <div class="inner-modal">
+                              <div id="logo" class="logo-modal"/>
+                              <div class="modal-title">{{treatments.harmonizacao.title}}</div>
+                              <div class="modal-text">{{treatments.harmonizacao.text}}</div>
+                              <button class="modal-close" v-on:click="hide('harmonizacao')">voltar</button>
+                         </div>
+                    </div>  
                </modal>
                <modal name="estetica" class="treatment-modal" :adaptive="true" width="100%" height="100%">
-                     <h1>{{treatments.estetica.title}}</h1>
-                     <div>{{treatments.estetica.text}}</div>
-                    <button  v-on:click="hide('estetica')">close</button>
+                    <div class="treatment-modal">
+                         <div class="inner-modal">
+                              <div id="logo" class="logo-modal"/>
+                              <div class="modal-title">{{treatments.estetica.title}}</div>
+                              <div class="modal-text">{{treatments.estetica.text}}</div>
+                              <button class="modal-close" v-on:click="hide('estetica')">voltar</button>
+                         </div>
+                    </div>
                </modal>
                <modal name="dtm" class="treatment-modal" :adaptive="true" width="100%" height="100%">
-                     <h1>{{treatments.dtm.title}}</h1>
-                     <div>{{treatments.dtm.text}}</div>
-                    <button  v-on:click="hide('dtm')">close</button>
+                     <div class="treatment-modal">
+                         <div class="inner-modal">
+                              <div id="logo" class="logo-modal"/>
+                              <div class="modal-title">{{treatments.dtm.title}}</div>
+                              <div class="modal-text">{{treatments.dtm.text}}</div>
+                              <button class="modal-close" v-on:click="hide('dtm')">voltar</button>
+                         </div>
+                    </div>
                </modal>
                <modal name="invisalign" class="treatment-modal" :adaptive="true" width="100%" height="100%">
-                     <h1>{{treatments.invisalign.title}}</h1>
-                     <div>{{treatments.invisalign.text}}</div>
-                    <button  v-on:click="hide('invisalign')">close</button>
+                     <div class="treatment-modal">
+                         <div class="inner-modal">
+                              <div id="logo" class="logo-modal"/>
+                              <div class="modal-title">{{treatments.invisalign.title}}</div>
+                              <div class="modal-text">{{treatments.invisalign.text}}</div>
+                              <button class="modal-close" v-on:click="hide('invisalign')">voltar</button>
+                         </div>
+                    </div>
                </modal>
                <modal name="implante" class="treatment-modal" :adaptive="true" width="100%" height="100%">
-                     <h1>{{treatments.implante.title}}</h1>
-                     <div>{{treatments.implante.text}}</div>
-                    <button  v-on:click="hide('implante')">close</button>
+                     <div class="treatment-modal">
+                         <div class="inner-modal">
+                              <div id="logo" class="logo-modal"/>
+                              <div class="modal-title">{{treatments.implante.title}}</div>
+                              <div class="modal-text">{{treatments.implante.text}}</div>
+                              <button class="modal-close" v-on:click="hide('implante')">voltar</button>
+                         </div>
+                    </div>
                </modal>
                <modal name="laserterapia" class="treatment-modal" :adaptive="true" width="100%" height="100%">
-                     <h1>{{treatments.laserterapia.title}}</h1>
-                     <div>{{treatments.laserterapia.text}}</div>
-                    <button  v-on:click="hide('laserterapia')">close</button>
+                     <div class="treatment-modal">
+                         <div class="inner-modal">
+                              <div id="logo" class="logo-modal"/>
+                              <div class="modal-title">{{treatments.laserterapia.title}}</div>
+                              <div class="modal-text">{{treatments.laserterapia.text}}</div>
+                              <button class="modal-close" v-on:click="hide('laserterapia')">voltar</button>
+                         </div>
+                    </div>
                </modal>
     </div>
 </template>
@@ -62,29 +92,29 @@ export default {
           return {
                treatments: {
                     harmonizacao: {
-                    title: "Harmonização orofacial ",
-                    text: "O sucesso do momento! Fazemos uma minuciosa análise facial digital para identificar os pontos de desarmonia em sua face e então traçamos um plano de tratamento totalmente personalizado, contando com aplicações de toxina botulínica (famoso Botox), preenchimento com ácido hialurônico, fios de sustentação e bioestimuladores de colágeno como IPRF, Sculptra, Elance e Radiesse para alcançarmos uma face totalmente harmônica e rejuvenescida!"
-               },
-               estetica: {
-               title: "Estética dental",
-               text: "Por meio de protocolos restauradores de excelência, realizamos clareamento dental, facetas cerâmica, lentes de contato, onlays, inlays e coroas minimalistas, a fim de te presentear com aquele sorriso perfeito do jeito que você sempre sonhou!"  
-               },
-               dtm: {
-                    title: "DTM e Dores orofaciais",
-                    text: "Você sabia que enxaquecas, bruxismo, insônia, falta de concentração, irritabilidade, fraturas espontâneas de dentes e dores irradiadas em fundo de olho, coluna ou até mesmo zumbidos e estalidos no ouvido podem ser sintomas de DTM? E tudo isso é desencadeado pelo stress da rotina moderna! Temos o tratamento ideal para você dar adeus a todos esses sintomas desagradáveis! Tenha um vida mais leve e feliz!"
-               },
-               invisalign: {
-                    title: "Aparelhos estéticos ",
-                    text: "Que tal conseguir ver o resultado final do tratamento ortodôntico antes mesmo de colocar o aparelho? Pois é! Com o iTero, sistema de scaneamento digital, é possível! A partir daí conseguimos corrigir o alinhamento dos dentes com um aparelho totalmente estético, por meio de uma sequência de alinhadores transparentes, super confortáveis, que não interferem na sua fala e podem ser retirados nos momentos das refeições e higienização dos dentes! Essa é a tecnologia que só a Invisalign oferece! Com conforto, previsibilidade e segurança ! Agora é fácil ter um sorriso perfeito com dentes alinhados!"
-               },
-               implante: {
-                    title: "Implante",
-                    text: "Conseguimos devolver um único elemento dental perdido ou ainda reabilitar a boca inteira com implantes super estéticos e resistentes! Devolvendo auto-estima e conforto, além de prevenir futuros problemas sistêmicos causados pela ausência de dentes! "
-               },
-               laserterapia: {
-                    title: "Laserterapia",
-                    text: "Através de aplicações rápidas e totalmente indolores de laser de baixa potência, conseguimos controlar infeções, reduzir processos inflamatórios, curar lesões bucais como aftas e herpes, tratar dores faciais relacionadas aos músculos ou ATM. Revertemos quadros de parestesia e paralisias faciais, proporcionamos pós operatórios cirúrgicos muito mais rápidos e confortáveis. De maneira extremamente biológica e sem efeitos colaterais! Laser é vida! Apaixone-se você também! "
-               }
+                         title: "Harmonização orofacial ",
+                         text: "O sucesso do momento! Fazemos uma minuciosa análise facial digital para identificar os pontos de desarmonia em sua face e então traçamos um plano de tratamento totalmente personalizado, contando com aplicações de toxina botulínica (famoso Botox), preenchimento com ácido hialurônico, fios de sustentação e bioestimuladores de colágeno como IPRF, Sculptra, Elance e Radiesse para alcançarmos uma face totalmente harmônica e rejuvenescida!"
+                    },
+                    estetica: {
+                         title: "Estética dental",
+                         text: "Por meio de protocolos restauradores de excelência, realizamos clareamento dental, facetas cerâmica, lentes de contato, onlays, inlays e coroas minimalistas, a fim de te presentear com aquele sorriso perfeito do jeito que você sempre sonhou!"  
+                    },
+                    dtm: {
+                         title: "DTM e Dores orofaciais",
+                         text: "Você sabia que enxaquecas, bruxismo, insônia, falta de concentração, irritabilidade, fraturas espontâneas de dentes e dores irradiadas em fundo de olho, coluna ou até mesmo zumbidos e estalidos no ouvido podem ser sintomas de DTM? E tudo isso é desencadeado pelo stress da rotina moderna! Temos o tratamento ideal para você dar adeus a todos esses sintomas desagradáveis! Tenha um vida mais leve e feliz!"
+                    },
+                    invisalign: {
+                         title: "Aparelhos estéticos ",
+                         text: "Que tal conseguir ver o resultado final do tratamento ortodôntico antes mesmo de colocar o aparelho? Pois é! Com o iTero, sistema de scaneamento digital, é possível! A partir daí conseguimos corrigir o alinhamento dos dentes com um aparelho totalmente estético, por meio de uma sequência de alinhadores transparentes, super confortáveis, que não interferem na sua fala e podem ser retirados nos momentos das refeições e higienização dos dentes! Essa é a tecnologia que só a Invisalign oferece! Com conforto, previsibilidade e segurança ! Agora é fácil ter um sorriso perfeito com dentes alinhados!"
+                    },
+                    implante: {
+                         title: "Implante",
+                         text: "Conseguimos devolver um único elemento dental perdido ou ainda reabilitar a boca inteira com implantes super estéticos e resistentes! Devolvendo auto-estima e conforto, além de prevenir futuros problemas sistêmicos causados pela ausência de dentes! "
+                    },
+                    laserterapia: {
+                         title: "Laserterapia",
+                         text: "Através de aplicações rápidas e totalmente indolores de laser de baixa potência, conseguimos controlar infeções, reduzir processos inflamatórios, curar lesões bucais como aftas e herpes, tratar dores faciais relacionadas aos músculos ou ATM. Revertemos quadros de parestesia e paralisias faciais, proporcionamos pós operatórios cirúrgicos muito mais rápidos e confortáveis. De maneira extremamente biológica e sem efeitos colaterais! Laser é vida! Apaixone-se você também! "
+                    }
                }
           }
      }
@@ -97,6 +127,71 @@ export default {
      background-color: #FFEDF0;
 }
 
+.treatment-modal {
+     width: 100%;
+     height: 100%;
+     flex-direction: column;
+     background-color: #FFEDF0;
+     color: white;
+     display: flex;
+     justify-content: center;
+     align-items: center;
+}
+
+.inner-modal {
+     width: 100%;
+     height: 100%;
+     color: #670C1B;;
+     display: flex;
+     justify-content: center;
+     align-items: center;
+     flex-direction: column;
+}
+
+.logo-modal {
+     margin: 0 !important;
+}
+
+.modal-title {
+     font-weight: 400;
+     font-size: 48px;
+     margin-bottom: 32px;
+     text-align: left;
+     padding-bottom: 64px;
+     border-bottom: 8px solid #670C1B;
+     border-radius: 50%;
+}
+
+.modal-text {
+     font-size: 24px;
+     width: 40%;
+     line-height: 1.6;
+     text-align: center;
+     margin-bottom: 40px;
+}
+
+.modal-close {
+     min-width: 120px;
+     padding: 8px;
+     border-radius: 4px;
+     font-size: 24px;
+     border: none;
+     background-color: #670C1B;
+     color: white;
+     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+     cursor: pointer;
+}
+
+.modal-close:hover {
+     min-width: 120px;
+     padding: 8px;
+     border-radius: 4px;
+     font-size: 24px;
+     border: none;
+     color: white;
+     background: #470812;
+     box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.35);
+}
 /* mobile */
 @media (min-width: 0px) {
      #treatment-title {
