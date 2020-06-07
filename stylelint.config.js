b0da1955f5013913fbaeccf6ed1704e49148061f
rafaelmaiach <rafael.chieregatto@hotmail.com>
2020-06-07 11:36:56 -0300

    feat: create contact section

--- stylelint.config.js
+++ stylelint.config.js
@@ -8,5 +8,8 @@ module.exports = {
 		'shorthand-property-no-redundant-values': null,
 		'length-zero-no-unit': null,
 		'declaration-property-value-blacklist': null,
+		'max-nesting-depth': null,
+		'selector-max-compound-selectors': null,
+		'selector-class-pattern': null,
 	},
 };
