1f9a4e1b9a4100cea1e0e36f1e0c60fe9f818577
rafaelmaiach <rafael.chieregatto@hotmail.com>
2020-06-11 13:26:56 -0300

    refactor: adds form to a new component

---
+++ components/Form/Form.vue
@@ -0,0 +1,80 @@
+<template>
+	<form class="form">
+		<label for="name" class="form__field">
+			Nome
+			<input id="name" type="text">
+		</label>
+
+		<label for="email" class="form__field">
+			E-mail
+			<input id="email" type="text">
+		</label>
+
+		<label for="tel" class="form__field">
+			Telefone
+			<input id="tel" type="text">
+		</label>
+
+		<label for="message" class="form__field">
+			Mensagem
+			<textarea id="message" name="message" rows="5" />
+		</label>
+		<button type="submit" class="submit">
+			Enviar
+		</button>
+	</form>
+</template>
+
+<script>
+	export default {
+		name: 'Form',
+	};
+</script>
+
+<style lang="scss" scoped>
+	.form {
+		display: flex;
+		flex-direction: column;
+
+		.form__field {
+			display: flex;
+			flex-direction: column;
+			margin-bottom: 16px;
+
+			input,
+			textarea {
+				border-radius: 3px;
+				border: 2px solid $light-red-03;
+				margin-top: 6px;
+
+				&:focus {
+					outline-color: $red-01;
+				}
+			}
+
+			textarea {
+				resize: none;
+			}
+		}
+
+		.submit {
+			font-size: 0.85rem;
+			border-radius: 3px;
+			padding: 4px 8px;
+			width: 120px;
+			border: 2px solid $light-red-03;
+			color: $red-01;
+			align-self: flex-end;
+			transition: background-color 0.3s linear;
+
+			&:hover {
+				cursor: pointer;
+			}
+
+			&:hover,
+			&:focus {
+				background-color: $light-red-03;
+			}
+		}
+	}
+</style>
